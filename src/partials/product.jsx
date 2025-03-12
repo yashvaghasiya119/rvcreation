540d2d411dc1b7987b1ba5be41a989facc93047a
yashvaghasiya119 <yashvaghasiya360@gmail.com>
2025-03-12 19:06:35 +0530

    a

--- src/partials/product.jsx
+++ src/partials/product.jsx
@@ -7,11 +7,11 @@ export function Product() {
             <div className="main-product-box">
                 {data.map((c, i) => {
                     return <div className="box">
-                        <img src={c.img} alt="" />
+                        <img src={require('/public/products/purse.jpg')} alt="" />
                         <h1>Product: {c.name}</h1>
                         <h3>Price: {c.price}</h3>
                         <h4>{c.description}</h4>
-                        
+
                         <button className="btn-explore">Buy now</button>
                     </div>
                 })}
